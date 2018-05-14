6cd7cd2d2e3ee61682f6bd2552fefb053f6478b0
FedericoMarras <justoxion@gmail.com>
2018-05-14 09:22:18 +0200

    Aggiungere i file di progetto.

---
+++ Menu/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Menu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Menu")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta il valore di ComVisible su falso, i tipi nell'assembly non sono più visibili
+// dai componenti COM. Se è necessario accedere al tipo nell'assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID di typelib
+[assembly: Guid("d91bd6c5-157d-4101-a6ff-7a54e351a56f")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero build
+//      Revisione
+//
+// È possibile specificare tutti i valori o lasciare i valori predefiniti per Revisione e Numeri build
+// utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
